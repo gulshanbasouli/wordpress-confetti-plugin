2a6c1067a02167792322b5240bd28697f9fd5bd5
Gulshan Chauhan <32733940+gulshanbasouli@users.noreply.github.com>
2026-06-09 01:59:16 +0530

    Refactor process_mgConfetti_option for better security
    Updated the process_mgConfetti_option function to improve security and variable handling.

--- inc/process.php
+++ inc/process.php
@@ -1,56 +1,79 @@
 <?php
-function process_mgConfetti_option()
-{
-   if ( !current_user_can( 'manage_options' ) )
-   {
-     wp_die(
-    __( 'You are not allowed to be on this page.', 'minigiv-confetti' )
-);
-   }
-   // Check that nonce field
-   check_admin_referer( 'mg_confetti_verify' );
 
-   $options = get_option( 'mg_confetti_array' );
-
-   if ( isset( $_POST['confetti_price'] ) ) {
-        $options['mg_confetti_price'] = absint(
-       $_POST['confetti_price']
-   );
-   }
-   if ( isset( $_POST['confetti_discount'] ) ) {
-     $options['mg_confetti_discount'] =
-    sanitize_text_field(
-        wp_unslash(
-            $_POST['confetti_discount']
-        )
-    );
-   }
-
-   $allowed_status = array(
-    'enabled',
-    'disabled'
-);
-
-if (
-    isset($_POST['confetti_status']) &&
-    in_array(
-        $_POST['confetti_status'],
-        $allowed_status,
-        true
-    )
-) {
-    $options['mg_confetti_status'] =
-        $_POST['confetti_status'];
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-   update_option( 'mg_confetti_array', $options );
+function process_mgConfetti_option() {
 
-   wp_safe_redirect(
-    admin_url(
-        'options-general.php?page=minigiv-confetti/inc/menus.inc.php_display_confetti&m=1'
-    )
-);
+	if ( ! current_user_can( 'manage_options' ) ) {
 
-   
+		wp_die(
+			esc_html__(
+				'You are not allowed to be on this page.',
+				'minigiv-confetti'
+			)
+		);
+
+	}
+
+	// Verify nonce.
+	check_admin_referer( 'mg_confetti_verify' );
+
+	$options = get_option( 'mg_confetti_array', array() );
+
+	// Confetti Price.
+	if ( isset( $_POST['confetti_price'] ) ) {
+
+		$options['mg_confetti_price'] = absint(
+			wp_unslash(
+				$_POST['confetti_price']
+			)
+		);
+
+	}
+
+	// Discount Code.
+	if ( isset( $_POST['confetti_discount'] ) ) {
+
+		$options['mg_confetti_discount_code'] = sanitize_text_field(
+			wp_unslash(
+				$_POST['confetti_discount']
+			)
+		);
+
+	}
+
+	// Confetti Status.
+	$allowed_status = array(
+		'enabled',
+		'disabled',
+	);
+
+	if ( isset( $_POST['confetti_status'] ) ) {
+
+		$status = sanitize_text_field(
+			wp_unslash(
+				$_POST['confetti_status']
+			)
+		);
+
+		if ( in_array( $status, $allowed_status, true ) ) {
+
+			$options['mg_confetti_status'] = $status;
+
+		}
+	}
+
+	// Save options.
+	update_option( 'mg_confetti_array', $options );
+
+	// Redirect back to settings page.
+	wp_safe_redirect(
+		admin_url(
+			'options-general.php?page=minigiv-confetti&m=1'
+		)
+	);
+
+	exit;
 }
-?>
