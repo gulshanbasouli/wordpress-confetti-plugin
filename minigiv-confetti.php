62028ea5a10c9d44edf5711ee7804f3473080802
Gulshan Chauhan <32733940+gulshanbasouli@users.noreply.github.com>
2026-06-09 01:26:20 +0530

    Refactor Minigiv Confetti plugin structure and metadata
    Updated plugin metadata, improved version check, and refactored functions for better readability.

--- minigiv-confetti.php
+++ minigiv-confetti.php
@@ -1,38 +1,48 @@
 <?php
-/*
-   * Plugin Name: Minigiv Confetti
-   *Author: Webchefz
-   *Author URI: https://www.webchefz.com
-   *Description: Manage Confetti
-   *Plugin URI:  https://github.com/gulshanbasouli/wordpress-confetti-plugin
-
-   Version: 0.1
+/**
+ * Plugin Name: Minigiv Confetti
+ * Plugin URI: https://github.com/gulshanbasouli/wordpress-confetti-plugin
+ * Description: Display confetti animations based on custom conditions.
+ * Version: 1.0.0
+ * Author: Gulshan Chauhan
+ * Author URI: https://loeion.com
+ * License: GPL v2 or later
+ * Text Domain: minigiv-confetti
  */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
+
+
 add_action( 'admin_post_mgConfetti_save_option', 'process_mgConfetti_option' );
 
 // Set up our WordPress Plugin
-function mg_checkVersion()
+function mg_confetti_check_version()
 {
-   if ( version_compare( get_bloginfo( 'version' ), '3.1', '<' ) ) {
-      wp_die("You must update WordPress to use this plugin!");
+   if ( version_compare( get_bloginfo( 'version' ), '6.0', '<' ) ) {
+      wp_die(
+    __( 'You must update WordPress to use this plugin!', 'minigiv-confetti' )
+);
    }
 
    if ( get_option( 'mg_confetti_array' ) === false ) {
 
       $options_array['mg_confetti_price'] = '3';
-      $options_array['mg_confetti_discount'] = 'coupon-code';
-      $options_array['mg_confetti_discount'] = '2';
+       $options_array['mg_confetti_discount_code'] = 'coupon-code';
+      $options_array['mg_confetti_discount_percent'] = '2';
 
       $options_array['mg_confetti_op_version'] = '1';
       add_option( 'mg_confetti_array', $options_array );
    }
 }
-register_activation_hook( __FILE__, 'mg_checkVersion' );
+register_activation_hook( __FILE__, 'mg_confetti_check_version' );
 
 // Include or Require any files
-include('inc/process.php');
-include('inc/display-options.inc.php');
-include('inc/menus.inc.php');
+require_once plugin_dir_path( __FILE__ ) . 'inc/process.php';
+require_once plugin_dir_path( __FILE__ ) . 'inc/display-options.inc.php';
+require_once plugin_dir_path( __FILE__ ) . 'inc/menus.inc.php';
+
 
 // Action & Filter Hooks
 add_action( 'admin_menu', 'mgconfetti_add_admin_menu' );
@@ -40,12 +50,17 @@ add_action( 'admin_menu', 'mgconfetti_add_admin_menu' );
 
 
 
-function getConfettiData(){
-   $arr = get_option( 'mg_confetti_array' );
-   print_r($arr);
+function mg_confetti_get_data() {
+    $arr = get_option( 'mg_confetti_array' );
+
+    ob_start();
+    echo '<pre>';
+    print_r( $arr );
+    echo '</pre>';
+
+    return ob_get_clean();
 }
 
 
-add_shortcode('confetti-data', 'getConfettiData');
-
+add_shortcode('confetti-data', 'mg_confetti_get_data');
 ?>
